fb9b70b465e245ff811f8fc82cddf386405e517a
Mateus Belfort <mateusaguia96@gmail.com>
2023-07-04 19:46:44 -0300

    update code
    incluse other code and script for the clock, include a background e a glass morphism in the clock background.

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function rotateClockHands() {
   const minute = now.getMinutes();
   const second = now.getSeconds();
 
-  const hourHand = document.querySelector('.hour-hand');
-  const minuteHand = document.querySelector('.minute-hand');
-  const secondHand = document.querySelector('.second-hand');
+  const hourHand = document.querySelector('.hour');
+  const minuteHand = document.querySelector('.minute');
+  const secondHand = document.querySelector('.second');
 
   const hourRotation = (hour * 30) + (minute * 0.5);
   const minuteRotation = (minute * 6) + (second * 0.1);
